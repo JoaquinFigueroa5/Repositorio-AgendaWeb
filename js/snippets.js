b8ad9b86c25ff42ce2b1cfe039f264379c4b77a7
JoaquinFigueroa5 <hfigueroa-2023417@kinal.edu.gt>
2025-01-21 16:21:16 -0600

    User terminado

--- js/snippets.js
+++ js/snippets.js
@@ -10,23 +10,8 @@ function redirectToPage(event) {
         window.location.href = "contacts.html";
     }
 
-/*    if (remember) {
-        localStorage.setItem("useername", username)
-        localStorage.setItem("password", password)
-    } else {
-        localStorage.removeItem("username")
-    }
-
-}
-
-window.onload = function (){
-    const savedUsername = localStorage.getItem('input[placeholder="Username"]');
-    const savedPassword = localStorage.getItem('input[placeholder="Password"]')
-    if (savedUsername && savedPassword) {
-        document.getElementById("username").value = savedUsername;
-        document.getElementByI("rememberMe").checked = true;
-    }
-        */
+    sessionStorage.setItem('username', username);
+    sessionStorage.setItem('password', password);
 }
 
 (() => {
@@ -63,7 +48,7 @@ window.onload = function (){
   const removePhoto = () => {
     const photoPreview = document.getElementById("photo-preview");
     photoPreview.innerHTML = `<span class="text-light">No Photo</span>`;
-    document.getElementById("upload-photo").value = ""; // Resetea el input
+    document.getElementById("upload-photo").value = "";
   };
   
   function showAlert(alertId){
@@ -73,4 +58,19 @@ window.onload = function (){
   function closeAlert(alertId){
     document.getElementById(alertId).style.display = "none";
   }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const username = sessionStorage.getItem('username');
+    const password = sessionStorage.getItem('password');
+
+    const listItems = document.querySelectorAll('.list-group-item');
+    if (username) {
+      listItems[1].textContent = username;
+    }
+    
+    if (password) {
+      listItems[2].textContent = password;
+    }
+
+  });
   
